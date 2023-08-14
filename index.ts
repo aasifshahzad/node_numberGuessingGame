70c710ad198f4975aaf4dba289dbd2c45717c02c
aasifshahzad <71362700+aasifshahzad@users.noreply.github.com>
2023-08-15 00:12:38 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import inquirer from "inquirer";
+import inquirer from "inquirer";   // import modules
 import chalk from "chalk";
 
 
-function setNum(min: number, max: number): number {
-    const randomFraction: number = Math.random();
+function setNum(min: number, max: number): number { // set the minimum and maximum limit for guessing th number
+    const randomFraction: number = Math.random();   // produce a random number
     const randomNumber: number = min + (randomFraction * (max - min));
     return randomNumber;
 }
@@ -13,12 +13,12 @@ async function startGame() {
         [
             {
                 type: "input",
-                message: chalk.bold.blueBright("Enter lower limit: "),
+                message: chalk.bold.blueBright("Enter lower limit: "),// user will enter lower limit
                 name: "lowerLimit",
             },
             {
                 type: "input",
-                message: chalk.bold.blueBright("Enter Upper Limit: "),
+                message: chalk.bold.blueBright("Enter Upper Limit: "),// user will enter upper limit
                 name: "upperLimit",
             },
 
@@ -26,21 +26,21 @@ async function startGame() {
     )
 
 
-    const lowerLimit: number = parseInt(inq1.lowerLimit);
+    const lowerLimit: number = parseInt(inq1.lowerLimit); //convert string into integer
     // console.log(lowerLimit)
 
-    const upperLimit: number = parseInt(inq1.upperLimit);
+    const upperLimit: number = parseInt(inq1.upperLimit); //convert string into integer
     // console.log(upperLimit);
-    console.log(chalk.bold.green(`Guess a number between/including ${chalk.bold.redBright(lowerLimit)} and ${chalk.bold.redBright(upperLimit)}.`));
+    console.log(chalk.bold.green(`Guess a number between/including ${chalk.bold.redBright(lowerLimit)} and ${chalk.bold.redBright(upperLimit)}.`)); //ask user to guess the number
 
 
-    const numSecret = parseInt(setNum(upperLimit, lowerLimit).toFixed())
+    const numSecret = parseInt(setNum(upperLimit, lowerLimit).toFixed()) //set the number to be guessed
     // console.log(numSecret);
     // console.log(typeof(numSecret));
 
 
     const maxGuesses = Math.ceil(Math.log2(upperLimit - lowerLimit + 1));
-    console.log(chalk.bold.green(`You have ${chalk.bold.redBright(maxGuesses)} guess to find out the number`));
+    console.log(chalk.bold.green(`You have ${chalk.bold.redBright(maxGuesses)} guess to find out the number`)); // set the maximum chanes to guess the number
 
     for (let index = 1; index < maxGuesses + 1; index++) {
         //console.log(index);
@@ -48,7 +48,7 @@ async function startGame() {
             [
                 {
                     type: "input",
-                    message: chalk.bold.blueBright("Guess the number:"),
+                    message: chalk.bold.blueBright("Guess the number:"), // user input for guess
                     name: "guess"
                 }
             ]
@@ -58,7 +58,7 @@ async function startGame() {
         // console.log(guess);
         // console.log(numSecret.toFixed());
 
-        console.log(chalk.bold.green(`You are left with ${chalk.bold.redBright(maxGuesses - index)} chance out of ${chalk.bold.redBright(maxGuesses)}.`));
+        console.log(chalk.bold.green(`You are left with ${chalk.bold.redBright(maxGuesses - index)} chance out of ${chalk.bold.redBright(maxGuesses)}.`)); // displaying the remaining chances
         if (guess > numSecret) {
             console.log(chalk.bold.red("You guess too High"));
         } else if (guess < numSecret) {
